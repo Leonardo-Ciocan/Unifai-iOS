17679ae2b7740bb89bd294da77b9c827e0cc5acc
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2016-05-28 04:56:46 +0100

    Missing files

---
+++ Unifai/MainSplitView.swift
@@ -0,0 +1,9 @@
+//
+//  MainSplitView.swift
+//  Unifai
+//
+//  Created by Leonardo Ciocan on 27/05/2016.
+//  Copyright © 2016 Unifai. All rights reserved.
+//
+
+import Foundation
---
+++ Unifai/MessageCreatorTextView.swift
@@ -0,0 +1,21 @@
+//
+//  MessageCreatorTextView.swift
+//  Unifai
+//
+//  Created by Leonardo Ciocan on 28/05/2016.
+//  Copyright © 2016 Unifai. All rights reserved.
+//
+
+import UIKit
+
+class MessageCreatorTextView: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

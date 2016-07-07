cb9a4f94c89cc70ea43f197a9a5f78a5c71e123b
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2016-07-07 11:31:31 +0100

    Catalog , UI changes , bug fixes

---
+++ Unifai/CatalogCell.swift
@@ -0,0 +1,18 @@
+//
+//  CatalogCell.swift
+//  Unifai
+//
+//  Created by Leonardo Ciocan on 05/07/2016.
+//  Copyright © 2016 Unifai. All rights reserved.
+//
+
+import UIKit
+
+class CatalogCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Unifai/CatalogItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  CatalogItemCell.swift
+//  Unifai
+//
+//  Created by Leonardo Ciocan on 05/07/2016.
+//  Copyright © 2016 Unifai. All rights reserved.
+//
+
+import UIKit
+
+class CatalogItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

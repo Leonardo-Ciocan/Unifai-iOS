b995e05413a55d034e44076c404f17c2a8263e94
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2016-07-14 19:43:16 +0100

    Revamped actions

---
+++ Unifai/ActionRunnerPage.swift
@@ -0,0 +1,21 @@
+//
+//  ActionRunnerPage.swift
+//  Unifai
+//
+//  Created by Leonardo Ciocan on 14/07/2016.
+//  Copyright © 2016 Unifai. All rights reserved.
+//
+
+import UIKit
+
+class ActionRunnerPage: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

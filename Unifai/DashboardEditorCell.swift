4a27e9471bde5cb94c2d369d3628e5c9101e49f4
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2016-08-14 02:19:42 +0100

    Dashboard usability updated , fixed hourly weather including auto weather

---
+++ Unifai/DashboardEditorCell.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardEditorCell.swift
+//  Unifai
+//
+//  Created by Leonardo Ciocan on 14/08/2016.
+//  Copyright © 2016 Unifai. All rights reserved.
+//
+
+import UIKit
+
+class DashboardEditorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
